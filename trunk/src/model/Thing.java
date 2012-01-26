20e9df459a376a0ae7a99aa51bf2e24e042f57ec
cyber19rider@gmail.com <cyber19rider@gmail.com@db36bbf3-1d47-7793-0b4c-406a2412fa5c>
2012-01-26 11:22:02 +0000

    CRUD review1

--- trunk/src/model/Thing.java
+++ trunk/src/model/Thing.java
@@ -5,136 +5,122 @@ import java.lang.reflect.Field;
 public class Thing {
 	private int tID;
 
-	public Thing(int tID) {
+	/**
+	 * Constructor that initializes a Thing using a tID 
+	 * @param tID	ID of the Thing
+	 */
+	public Thing(int tID)
+	{
 		this.settID(tID);
 	}
-	
-	public Thing() // DO NOT DELETE: Breaks Subclasses constructors
+	/**
+	 * Constructor that initializes a Thing as a basic object
+	 * @warning DO NOT DELETE: Breaks Subclasses' constructors
+	 */
+	public Thing()
 	{
 		
 	}
 
 	/**
-	 * @return the tID
+	 * @return	ID of the Thing
 	 */
 	public int gettID() {
 		return tID;
 	}
 
 	/**
-	 * @param tID the tID to set
+	 * @param param tID	ID of the Thing
 	 */
 	public void settID(int tID) {
 		this.tID = tID;
 	}
-
+	
+	/**
+	 * Generates an SQL Insert query based on Thing's fields and Class name
+	 * <p> Example: insert into User values(1,1,2,3,4,5,6,7)
+	 * @return a String containing an SQL Insert query
+	 */
 	public String insertQuery()
 	{
 		String query = "insert into "; //starting with the "constant" start of the Query
 		//@warning If Table Name it's not the same as the name of the class, query fails
 		query += this.getClass().getSimpleName() + " "; //adding the Name of the Table!
 		query += "values("; // adding constant part of query
-		Field[] fields = this.getClass().getDeclaredFields(); //finding Fields present in a Class
-		for (Field f:fields) //looping through the fields
-		{
-			// IGNORE Exceptions for now
-			try {
-				query += f.get(this) +","; // gets the values of the field of "this" object
-			
-			} catch (IllegalArgumentException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} //adding fields separated by commas
-		}
-		query = query.substring(0,query.length()-1); //removing the last comma
+		query += sqlCondition(this,null,",");
 		query+=")"; //adding the last constant part of the query
 		return query;
 	}
 
-	//select * from User where username like '%2%' and password like '%3%' and type like '%4%'
+	/**
+	 * Generates a SQL select Query based on Thing's fields and Class name
+	 * <p> Example: select * from User where uID like '%1%' and name like '%user%' and password like '%password%'
+	 * @return a String containing a SQL Insert Query
+	 */
 	public String selectQuery() {
 		String query = "select * from "; // first constant of a Query
 		//	@warning If Table Name it's not the same as the name of the class, query fails
 		query += this.getClass().getSimpleName() + " "; //adding the Name of the Table!
 		query += "where "; //constant statement in SQL language
-		Field[] fields = this.getClass().getDeclaredFields(); //finding Fields present in a Class
-		for (Field f:fields) //looping through the fields
-		{
-			try {
-				if (f.get(this) != null && !f.get(this).toString().equals("0")) //avoid nulls and zeros for field's values
-				{
-					query += f.getName() + " ";
-					query += "like '%"; //constant statement in SQL language
-					query += f.get(this) + "%' "; // gets the values of the field of "this" object
-					query += "and "; //constant statement in SQL language
-				}
-			} catch (IllegalArgumentException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			} catch (IllegalAccessException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-		}
-		query = query.substring(0, query.length()-4); //removing the last "and " from the query
+		query += sqlCondition(this," like '%","%' and "); //generating the SQL condition using like and "and"
+		query += "%'"; //adding the missing part
 		return query;
 	}
 
-	/** UPDATE table_name
-	*   SET column1=value, column2=value2,...
-	*   WHERE some_column=some_value 
-	*/
+	/**
+	 * Generates a SQL update Query for updating the current Thing (this) with other Thing provided by parameter
+	 * <p> Example: update User set uID = 0, name = new_name, username = new_username where uID = 1, name = old_name, username = old_username
+	 * @param t	Thing that should replace the current Thing
+	 * @return a String containing a SQL update Query
+	 */
 	public String updateQuery(Thing t)
-	
 	{
 		String query = "update "; //constant part of query
 		query += this.getClass().getSimpleName() + " "; //add Table name
 		query += "set "; //constant part of query
-		Field[] fields = this.getClass().getDeclaredFields(); //finding Fields present in a Class
-		try
-		{
-			for (Field f:fields) //looping through the fields
-			{
-				query += f.getName() +"=" + f.get(this) + ", "; // constructs the statement column1=value
-			}
-			
-			query = query.substring(0, query.length()-2) + " "; //removing the last comma from the query
-			query += "where "; //constant part of query
-			
-			for (Field f:fields) //looping through the fields
-			{
-				query += f.getName() +"=" + f.get(t) + " and "; // constructs the statement column1=value
-			}
-			query = query.substring(0, query.length()-4); //removing the last "and " from the query
-		}
-		// IGNORE Exceptions for now
-		catch (IllegalArgumentException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		catch (IllegalAccessException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+		query += sqlCondition(t, " = ", ", "); //constructing the condition with the fields
+		query += " where "; //constant part of query
+		query += sqlCondition(this, " = ", ", ");
 		return query;
 	}
 
+	/**
+	 * Generates a SQL delete Query for the current Thing
+	 * <p> Example: delete from User where uID = 1 and name = 1 and username = 2 and password = 3 and type = 4 and ID = 5 and email = 6 and phone = 7
+	 * @return a String containing a SQL delete Query
+	 */
 	public String deleteQuery() {
 		String query = "delete from "; //constant part of query
 		query += this.getClass().getSimpleName() + " "; // Table Name
 		query += "where "; // constant
-		Field[] fields = this.getClass().getDeclaredFields(); //finding out fields
+		query += sqlCondition(this, " = ", " and ");
+		return query;
+	}
+	
+	/**
+	 * Generates a SQL condition from the object and one or two regular expressions
+	 * <p> Example: field_name regex1 field_value regex2 field_name regex1 field_value
+	 * <p> Last regex will always be deleted (because it is illegal in SQL language)
+	 * @param t - the Thing that the condition is applied for | could not be NULL
+	 * @param regex1 - string that represents what should be after field's names in a SQL Condition | if NULL, only values will be used
+	 * @param regex2 - string that represents what should be after field's values in a SQL Condition | if NULL, only names will be used
+	 * @return the resulted SQL condition
+	 */
+	private String sqlCondition(Thing t, String regex1, String regex2)
+	{
+		Field[] fields = t.getClass().getDeclaredFields(); //finding Fields present in a Class
+		String result = "";
 		for (Field f:fields)
 		{
 			try {
-				if (f.get(this) != null && !f.get(this).toString().equals("0"))
-					query += f.getName() +" = " + f.get(this) + " and ";
+				if (regex1 == null)
+					result += f.get(t) + regex2; // generate SQL condition using just values and regex2
+				
+				if (regex2 == null)
+					result += f.getName() + regex1; // generate SQL condition using just names and regex1;
+				
+				if (f.get(this) != null && !f.get(this).toString().equals("0") && regex1 != null && regex2 != null) //avoid nulls and zeros
+					result += f.getName() + regex1 + f.get(t) + regex2; // generate SQL condition from fields, thing and separator
 			} catch (IllegalArgumentException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -143,7 +129,9 @@ public class Thing {
 				e.printStackTrace();
 			} // constructs the statement column1=value
 		}
-		query = query.substring(0, query.length()-4);
-		return query;
+		if (regex2 != null)
+			result = result.substring(0, result.length()-regex2.length()); //cutting the last regex2 that appears in the query (it is illegal in SQL
+		else result = result.substring(0, result.length()-regex1.length()); //cutting the last regex1 that appears in the query (it is illegal in SQL
+		return result;
 	}
 }
